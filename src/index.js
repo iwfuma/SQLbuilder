7e699ead4c8d60c4877bdaacef0a0f772189b745
Iwami Fuma <sado24230023@nsgcl.jp>
2025-07-19 23:51:58 +0900

    fix: use HashRouter for GitHub Pages routing

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';  
+import { HashRouter } from 'react-router-dom';  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/SQLbuilder">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
